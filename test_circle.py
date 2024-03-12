fa2673b60c282190dc61fb077e6e4f2127a56709
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-03-12 15:48:34 +0700

    prob1

--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,39 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
+import unittest
 from circle import Circle
 
-# TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    """Tests of the Circle class."""
+
+    def setUp(self):
+        """Create a circle before each test."""
+        self.circle = Circle(1)
+        self.circle2 = Circle(10)
+
+    def test_add_area_typical_values(self):
+        """
+        Typical case:
+        Test add_area with two circles having positive radii.
+        """
+
+        result = self.circle.add_area(self.circle2)
+        self.assertEqual(result.get_radius(), 10.04987562112089)
+
+    def test_add_area_for_radius_zero(self):  # edge case
+        """
+        Edge case:
+        Test add_area when one of the circles has radius 0,
+        the other has non-zero radius.
+        (Result should have same radius.)
+        Does add_area work the same regardless of
+        which circle has radius 0?
+        """
+
+        self.circle_zero = Circle(0)
+        result = self.circle.add_area(self.circle_zero)
+        self.assertEqual(result.get_radius(), 1)
+
+
